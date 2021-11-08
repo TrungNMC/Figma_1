cbf67593ddfffb01adb9a668d3d7e4eef7920e04
NMCT <67629495+NMTC-K59@users.noreply.github.com>
2021-11-08 17:43:43 +0700

    done

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+$('.customer-items').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fas fa-arrow-left"></i></button>`,
+    nextArrow:`<button type='button' class='slick-next pull-right'><i class="fas fa-arrow-right"></i></button>`,
+  });
