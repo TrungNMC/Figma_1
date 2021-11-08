7b24978c59f6d2606e88128f6f8c789961333ab8
NMCT <67629495+NMTC-K59@users.noreply.github.com>
2021-11-08 22:10:08 +0700

    done

--- js/app.js
+++ js/app.js
@@ -8,3 +8,26 @@ $('.customer-items').slick({
     prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fas fa-arrow-left"></i></button>`,
     nextArrow:`<button type='button' class='slick-next pull-right'><i class="fas fa-arrow-right"></i></button>`,
   });
+
+var openNav = document.querySelector(".open-menu");
+var closeNav = document.querySelector(".close-menu");
+var navBar = document.querySelector(".nav-links");
+
+
+openNav.addEventListener("click", ()=> {
+    openNav.style.display = "none";
+    closeNav.style.display = "block";
+    navBar.style.opacity = "1";
+    navBar.style.visibility = "visible";
+    navBar.style.right = "22px";
+})
+
+
+closeNav.addEventListener("click", ()=> {
+    openNav.style.display = "block";
+    closeNav.style.display = "none";
+    navBar.style.opacity = "0";
+    navBar.style.visibility = "hidden";
+    navBar.style.right = "-50px";
+
+})
